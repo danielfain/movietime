ce84c75347b0324bb8fe8accdd3c9ec5ea815688
Daniel Fain <d6niel@protonmail.com>
2020-09-02 10:52:03 -0400

    Make component fully typed & add li key

--- src/Trending.tsx
+++ src/Trending.tsx
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const API_URL = process.env.REACT_APP_API_URL;
 const API_KEY = process.env.REACT_APP_API_KEY;
+const POSTER_URL = process.env.REACT_APP_POSTER_URL;
 
-type TrendingAPIResult = {
+type TrendingMedia = {
+  id: number;
   title: string;
   poster_path: string;
   vote_average: number;
-}
+};
+
+type TrendingAPIResponse = {
+  results: Array<TrendingMedia>;
+};
 
 type TrendingState = {
-  media: Array<TrendingAPIResult>;
-}
+  media: Array<TrendingMedia>;
+};
 
-type TrendingProps = {}
+type TrendingProps = {};
 
-class Trending extends Component<TrendingProps, TrendingState> {
+class Trending extends React.Component<TrendingProps, TrendingState> {
   constructor(props: TrendingProps) {
     super(props);
     this.state = {
@@ -23,27 +29,28 @@ class Trending extends Component<TrendingProps, TrendingState> {
     }
   }
 
-  componentDidMount() {
+  componentDidMount(): void {
     fetch(`${API_URL}/trending/all/week?api_key=${API_KEY}`)
       .then(res => res.json())
-      .then(data => {
-        let { results } = data;
+      .then((data: TrendingAPIResponse) => {
+        let results: Array<TrendingMedia> = data.results;
         results = results.slice(0, 5);
         this.setState({ media: results } );
       })
+      .catch(error => console.error(error));
   }
 
-  render() {
+  render(): JSX.Element {
     return (
       <div className="trending-container">
-        <h1 style={{ color: "azure" }}>Trending</h1>
+        <h1>Trending</h1>
         <div className="trending-content">
           <ul style={{ listStyleType: "none", margin: 0, padding: 0 }}>
             {this.state.media.map((item, index) => (
-              <li>
-                <img src={process.env.REACT_APP_POSTER_URL + item.poster_path}  alt={item.title} />
+              <li key={item.id}>
+                <img src={POSTER_URL + item.poster_path}  alt={item.title} />
                 <div className="trending-labels">
-                  <label style={{ color: "azure" }} >{index + 1}</label>
+                  <label style={{ color: "azure" }}>{index + 1}</label>
                   <label style={{ color: "gold" }}>{item.vote_average} / 10</label>
                 </div>
               </li>
