7f8c3f8cdf63e2dc2d3f3e3f0742efa04a39499d
Daniel Fain <d6niel@protonmail.com>
2020-09-03 10:11:29 -0400

    Add react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Layout from './Layout';
 
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Layout />
+    <BrowserRouter>
+      <Layout />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
